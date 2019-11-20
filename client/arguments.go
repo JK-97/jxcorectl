4f1dfce71560746bc2dd78895286952dfdf4f32f
JK-97 <benjamin-dream@outlook.com>
2019-11-21 03:27:22 +0800

    #added 加入了 resource command suggest

--- client/arguments.go
+++ client/arguments.go
@@ -1,1 +1,20 @@
 package client
+
+import ()
+import "github.com/c-bata/go-prompt"
+
+var commands = []prompt.Suggest{
+	{Text: "get", Description: "Display one or many resources"},
+	{Text: "describe", Description: "Show details of a specific resource or group of resources"},
+	{Text: "logs", Description: "Print the logs for a container in a pod."},
+	{Text: "start", Description: "start the program in jxcore"},
+	// Custom command.
+	{Text: "exit", Description: "Exit this program"},
+}
+
+var resourceTypes = []prompt.Suggest{
+	{Text: "jxserving"}, // valid only for federation apiservers
+	{Text: "db"},
+	{Text: "mq"},
+	{Text: "telegraf"},
+}
