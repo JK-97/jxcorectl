794e494bd3639bf5971c36a39c53a4bacd1a0a89
JK-97 <benjamin-dream@outlook.com>
2019-11-21 19:22:09 +0800

    #added rpc

--- client/client.go
+++ client/client.go
@@ -8,26 +8,24 @@ import (
 	"github.com/c-bata/go-prompt/completer"
 )
 
-func customcompleter(d prompt.Document) []prompt.Suggest {
-	s := []prompt.Suggest{
-		{Text: "users", Description: "Store the username and age"},
-		{Text: "articles", Description: "Store the article text posted by user"},
-		{Text: "comments", Description: "Store the text commented to articles"},
-	}
-	return prompt.FilterHasPrefix(s, d.GetWordBeforeCursor(), true)
-}
-
+//启动client
 func Run(version string) {
+	c := NewCompleter()
+	e := NewRpcExcutior(
+		"http://127.0.0.1:9001",
+		"root",
+		"",
+	)
 
 	defer debug.Teardown()
-	fmt.Printf("kube-prompt %s \n", version)
+	fmt.Printf("jxcorectl %s \n", version)
 	fmt.Println("Please use `exit` or `Ctrl-D` to exit this program.")
 	defer fmt.Println("Bye!")
 	p := prompt.New(
-		Executor,
-		customcompleter,
+		e.Execute,
+		c.completer,
 		prompt.OptionTitle("jxcore-prompt: interactive jxcore client"),
-		prompt.OptionPrefix(">>> "),
+		prompt.OptionPrefix("jxcorectl > "),
 		prompt.OptionInputTextColor(prompt.Yellow),
 		prompt.OptionCompletionWordSeparator(completer.FilePathCompletionSeparator),
 	)
--- go.mod
+++ go.mod
@@ -4,10 +4,15 @@ go 1.13
 
 require (
 	github.com/c-bata/go-prompt v0.2.3
+	github.com/gorilla/rpc v1.2.0 // indirect
+	github.com/jessevdk/go-flags v1.4.0
 	github.com/mattn/go-colorable v0.1.4 // indirect
 	github.com/mattn/go-runewidth v0.0.6 // indirect
 	github.com/mattn/go-tty v0.0.0-20191112051231-74040eebce08 // indirect
+	github.com/ochinchina/gorilla-xmlrpc v0.0.0-20171012055324-ecf2fe693a2c
+	github.com/ochinchina/supervisord v0.6.3
 	github.com/pkg/term v0.0.0-20190109203006-aa71e9d9e942 // indirect
+	github.com/rogpeppe/go-charset v0.0.0-20190617161244-0dc95cdf6f31 // indirect
 	github.com/spf13/cobra v0.0.5
 	golang.org/x/sys v0.0.0-20190312061237-fead79001313 // indirect
 )
